26e159ebb4b40c92a25f697857e444ac9b604fbf
Annie Chin <afchin@google.com>
2016-05-18 15:17:14 -0700

    Calculator multiwindow support.
    Bug: 27906424
    Change-Id: I9b9bf40be9909ce5def5762e69a21dd1ff94075b

--- src/com/android/calculator2/CalculatorPadViewPager.java
+++ src/com/android/calculator2/CalculatorPadViewPager.java
@@ -130,7 +130,7 @@ public class CalculatorPadViewPager extends ViewPager {
 
         setAdapter(mStaticPagerAdapter);
         setBackgroundColor(Color.BLACK);
-        setPageMargin(getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
+        setPageMargin(-getResources().getDimensionPixelSize(R.dimen.pad_page_margin));
         setPageTransformer(false, mPageTransformer);
         addOnPageChangeListener(mOnPageChangeListener);
     }
