f36ce9239ec10168727ed9b2769ddcf91882fcbd
Justin Klaassen <justinklaassen@google.com>
2017-01-27 09:09:40 -0800

    Add android-support-v7-recyclerview dependency
    - Also removed obsolete testing files.
    
    Test: tapas ExactCalculator && make -j8
    Change-Id: I87e525e978f76ab0ffbd4d9d13789e82f1693247

--- Android.mk
+++ Android.mk
@@ -21,8 +21,10 @@ LOCAL_RESOURCE_DIR := packages/apps/ExactCalculator/res
 
 ifeq ($(TARGET_BUILD_APPS),)
 LOCAL_RESOURCE_DIR += frameworks/support/v7/gridlayout/res
+LOCAL_RESOURCE_DIR += frameworks/support/v7/recyclerview/res
 else
 LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/gridlayout/res
+LOCAL_RESOURCE_DIR += prebuilts/sdk/current/support/v7/recyclerview/res
 endif
 
 LOCAL_MODULE_TAGS := optional
@@ -38,10 +40,10 @@ LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 LOCAL_STATIC_JAVA_LIBRARIES := cr
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
 LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-gridlayout
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v7-recyclerview
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.gridlayout
+LOCAL_AAPT_FLAGS += --extra-packages android.support.v7.recyclerview
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
