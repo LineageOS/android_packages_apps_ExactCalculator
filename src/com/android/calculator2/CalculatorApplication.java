17872c10c60f1d0e7416b192cec3aba1c3d99755
Georg Veichtlbauer <georg@vware.at>
2023-08-12 22:59:12 +0200

    ExactCalculator: Apply dynamic colors to activities
    Change-Id: I0b86b76b58f4695ddd5f6d5b55e1672346ee3029

---
+++ src/com/android/calculator2/CalculatorApplication.java
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.android.calculator2;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class CalculatorApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
