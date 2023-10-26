cb25277c8cac76bc5b9956db69ee24b85c6fcb29
Georg Veichtlbauer <georg@vware.at>
2023-10-26 15:34:31 +0200

    ExactCalculator: Migrate to from Groovy to Kotlin DSL
    Change-Id: I3e183fed6c642a47fa335da73adcf5fdaee472b9

---
+++ build.gradle.kts
@@ -0,0 +1,68 @@
+/*
+ * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+plugins {
+    id("com.android.application") version "8.1.2"
+    id("org.jetbrains.kotlin.android") version "1.7.10"
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.layout.buildDirectory)
+}
+
+android {
+    compileSdk = 34
+    namespace = "com.android.calculator2"
+
+    defaultConfig {
+        applicationId = "com.android.calculator2"
+        minSdk = 31
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            // Enables code shrinking, obfuscation, and optimization.
+            isMinifyEnabled = true
+
+            // Enables resource shrinking.
+            isShrinkResources = true
+
+            // Includes the default ProGuard rules files.
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android.txt"),
+                    "proguard.flags"
+                )
+            )
+        }
+        getByName("debug") {
+            // Append .dev to package name so we won't conflict with AOSP build.
+            applicationIdSuffix = ".dev"
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    sourceSets {
+        getByName("main") {
+            res.srcDirs("res")
+            java.srcDirs("src")
+            assets.srcDirs("assets")
+            manifest.srcFile("AndroidManifest.xml")
+        }
+    }
+}
+
+dependencies {
+    implementation(files("cr.jar"))
+
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("androidx.webkit:webkit:1.7.0")
+    implementation("com.google.android.material:material:1.9.0")
+}
