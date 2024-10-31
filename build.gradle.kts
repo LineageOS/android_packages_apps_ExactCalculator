c0e11499171bfeb5d03e9ffc560d09e1e0dea200
Aayush Gupta <aayushgupta219@gmail.com>
2024-10-31 13:26:20 +0530

    Bump to latest stable AGP, Gradle and dependencies
    Change-Id: I504904003d7b8946ec604ed6480a1fe1cfeadb42
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,8 +8,8 @@ import org.lineageos.generatebp.GenerateBpPluginExtension
 import org.lineageos.generatebp.models.Module
 
 plugins {
-    id("com.android.application") version "8.1.2"
-    id("org.jetbrains.kotlin.android") version "1.7.10"
+    id("com.android.application") version "8.7.1"
+    id("org.jetbrains.kotlin.android") version "1.9.23"
 }
 
 apply {
@@ -18,7 +18,7 @@ apply {
 
 buildscript {
     repositories {
-        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.4/.m2")
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.13/.m2")
     }
 
     dependencies {
@@ -75,8 +75,8 @@ android {
 
 dependencies {
     implementation("androidx.gridlayout:gridlayout:1.0.0")
-    implementation("androidx.webkit:webkit:1.7.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.webkit:webkit:1.7.0-alpha02")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("com.hp:crcalc:1.0")
 }
 
