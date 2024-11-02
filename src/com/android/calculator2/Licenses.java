3366065e3d6ff538106247c475b0f86d12aac81c
Aayush Gupta <aayushgupta219@gmail.com>
2024-11-02 18:23:12 +0530

    ExactCalculator: Switch to dedicated layout for license activity
    Change-Id: I61b16816afc5051d6e7abf2750c62a3e0475eadc
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- src/com/android/calculator2/Licenses.java
+++ src/com/android/calculator2/Licenses.java
@@ -6,10 +6,10 @@
 package com.android.calculator2;
 
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.webkit.WebView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Licenses extends AppCompatActivity {
 
@@ -18,19 +18,12 @@ public class Licenses extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
 
-        final WebView webView = new WebView(this);
+        Toolbar toolbar = requireViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+
+        WebView webView = requireViewById(R.id.webview);
         webView.loadUrl(LICENSE_URL);
-
-        setContentView(webView);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            onBackPressed();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
