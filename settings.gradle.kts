ce30d0c316f99205b9fc1ef501ad0de32676d59d
Luca Stefani <luca.stefani.ge1@gmail.com>
2025-01-10 11:50:28 +0100

    ExactCalculator: Migrate generatebp to plugin declaration
    Change-Id: I38020976a6414c297ff37bbba7d2c7d1aaa4423f

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: 2023-2025 The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,8 +8,10 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven("https://raw.githubusercontent.com/lineage-next/gradle-generatebp/v1.21/.m2")
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -18,4 +20,5 @@ dependencyResolutionManagement {
         maven("https://raw.githubusercontent.com/lineage-next/crcalc/main/.m2")
     }
 }
+
 rootProject.name = "ExactCalculator"
