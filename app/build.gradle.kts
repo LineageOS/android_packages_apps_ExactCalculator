0afa4f3eaf19697fcd9695d7f4ea2431a1961329
pnguyen879 <howardson2001@gmail.com>
2026-03-10 21:38:35 -0500

    ExactCalculator: Reorganize project structure and files for Android Studio compatibility
    - Move all files into their respective parts
    - Migrate all plugins and implementations to the Gradle Version Catalog
    - Align a few Gradle files similar to Jelly's files
    
    Change-Id: I0f7ebc7a41d3607f0e272ebd270798b85fa7561b

---
+++ app/build.gradle.kts
@@ -0,0 +1,74 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2026 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import org.lineageos.generatebp.GenerateBpPluginExtension
+import org.lineageos.generatebp.models.Module
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.lineageos.generatebp)
+}
+
+android {
+    namespace = "com.android.calculator2"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.android.calculator2"
+        minSdk = 31
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            // Enables code shrinking, obfuscation, and optimization.
+            isMinifyEnabled = true
+
+            // Enables resource shrinking.
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard.files"
+            )
+        }
+
+        debug {
+            // Append .dev to package name so we won't conflict with AOSP build.
+            applicationIdSuffix = ".dev"
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.gridlayout)
+    implementation(libs.androidx.webkit)
+    implementation(libs.material)
+    implementation(libs.crcalc)
+}
+
+configure<GenerateBpPluginExtension> {
+    targetSdk.set(android.defaultConfig.targetSdk!!)
+    minSdk.set(android.defaultConfig.minSdk!!)
+    availableInAOSP.set { module: Module ->
+        when {
+            module.group.startsWith("androidx") -> true
+            module.group.startsWith("org.jetbrains") -> true
+            module.group == "com.google.android.material" -> true
+            module.group == "com.google.errorprone" -> true
+            module.group == "com.google.guava" -> true
+            module.group == "junit" -> true
+            else -> false
+        }
+    }
+}
