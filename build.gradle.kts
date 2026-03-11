8b6bdd074d044d7e8d9acbc815865bcfeefe6cba
pnguyen879 <howardson2001@gmail.com>
2026-03-10 21:38:35 -0500

    ExactCalculator: Reorganize project structure and files for Android Studio compatibility
    - Move all files into their respective parts
    - Migrate all plugins and implementations to the Gradle Version Catalog
    - Align a few Gradle files similar to Jelly's files
    
    Change-Id: I0f7ebc7a41d3607f0e272ebd270798b85fa7561b

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,85 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2023 The LineageOS Project
+ * SPDX-FileCopyrightText: The LineageOS Project
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import org.lineageos.generatebp.GenerateBpPluginExtension
-import org.lineageos.generatebp.models.Module
-
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.7.1"
-    id("org.jetbrains.kotlin.android") version "1.9.23"
-    id("org.lineageos.generatebp") version "+"
-}
-
-android {
-    compileSdk = 35
-    namespace = "com.android.calculator2"
-
-    defaultConfig {
-        applicationId = "com.android.calculator2"
-        minSdk = 31
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-    }
-
-    buildTypes {
-        getByName("release") {
-            // Enables code shrinking, obfuscation, and optimization.
-            isMinifyEnabled = true
-
-            // Enables resource shrinking.
-            isShrinkResources = true
-
-            // Includes the default ProGuard rules files.
-            setProguardFiles(
-                listOf(
-                    getDefaultProguardFile("proguard-android.txt"),
-                    "proguard.flags"
-                )
-            )
-        }
-        getByName("debug") {
-            // Append .dev to package name so we won't conflict with AOSP build.
-            applicationIdSuffix = ".dev"
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    sourceSets {
-        getByName("main") {
-            res.srcDirs("res")
-            java.srcDirs("src")
-            assets.srcDirs("assets")
-            manifest.srcFile("AndroidManifest.xml")
-        }
-    }
-}
-
-dependencies {
-    implementation("androidx.gridlayout:gridlayout:1.0.0")
-    implementation("androidx.webkit:webkit:1.7.0-alpha02")
-    implementation("com.google.android.material:material:1.14.0-alpha09")
-    implementation("com.hp:crcalc:1.0")
-}
-
-configure<GenerateBpPluginExtension> {
-    targetSdk.set(android.defaultConfig.targetSdk!!)
-    minSdk.set(android.defaultConfig.minSdk!!)
-    versionCode.set(android.defaultConfig.versionCode!!)
-    versionName.set(android.defaultConfig.versionName!!)
-    availableInAOSP.set { module: Module ->
-        when {
-            module.group.startsWith("androidx") -> true
-            module.group.startsWith("org.jetbrains") -> true
-            module.group == "com.google.android.material" -> true
-            module.group == "com.google.errorprone" -> true
-            module.group == "com.google.guava" -> true
-            module.group == "junit" -> true
-            else -> false
-        }
-    }
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.lineageos.generatebp) apply false
 }
